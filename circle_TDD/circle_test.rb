1ef110b1c694b3f6e71ef6356dfd2ff28f0d4267
Eric Xu <e.xulander@gmail.com>
2013-11-01 22:48:46 -0400

    setup circle class and minitest specs

---
+++ circle_TDD/circle_test.rb
@@ -0,0 +1,14 @@
+require_relative "circle"
+require "minitest/autorun"
+
+
+
+# ruby circle_test.rb --verbose
+# or use 'rake test' with a Rakefile
+
+	# require "rake/testtask"
+	# Rake::TestTask.new do |t|
+	# 	t.libs << "test"
+	# 	t.pattern = "test/*_test.rb"
+	# 	t.verbose = true
+	# end
